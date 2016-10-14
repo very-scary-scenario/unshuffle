f64752b2ced17a7981705515fc8ba5a58b873c95
colons <code@colons.co>
2016-10-14 20:18:06 +0100

    Be 'unshuffle', for now.

--- unshuffle/urls.py
+++ unshuffle/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^', include('order.apps.ui.urls')),
+    url(r'^', include('unshuffle.apps.ui.urls')),
 ]
--- unshuffle/wsgi.py
+++ unshuffle/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for order project.
+WSGI config for unshuffle project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "order.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unshuffle.settings")
 
 application = get_wsgi_application()
