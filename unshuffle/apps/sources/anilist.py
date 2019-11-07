ab77d2bcaf6a25deea5fc9df57f0cc5e9ba0a12b
colons <colons@colons.co>
2019-11-07 15:21:08 +0000

    Fix anilist decks.

--- unshuffle/apps/sources/anilist.py
+++ unshuffle/apps/sources/anilist.py
@@ -3,103 +3,93 @@ from datetime import datetime
 import requests
 
 from django.conf import settings
+from django.core.cache import cache
 
 from ..sources import source, cached
 
 
-AUTH_PARAMS = {}
-URL_FMT = 'https://anilist.co/api/{}'
+URL = 'https://graphql.anilist.co'
 SEASONS = ['winter', 'spring', 'summer', 'fall']
+POPULAR_ANIME = []
 
 
-def _anilist(method, **params):
-    return requests.get(
-        URL_FMT.format(method),
-        params={
-            **AUTH_PARAMS,
-            **params,
-        },
-    )
+def _popular_anime(*a, **k):
+    ck = 'anilist:popular'
+    hit = cache.get(ck)
+    if hit:
+        yield from hit
+    else:
+        for page in range(1, 20):
+            resp = requests.post(URL, json={
+                'query': """
+                query ($page: Int) {
+                    Page (perPage: 50, page: $page) {
+                        pageInfo {
+                            hasNextPage
+                        }
+                        media (type: ANIME, popularity_greater: 20000) {
+                            title {
+                                romaji
+                                english
+                            }
+                            startDate {
+                                year
+                            }
+                            genres
+                            averageScore
+                        }
+                    }
+                }
+                """,
+                'variables': {'page': page},
+            }).json()
+            yield from resp['data']['Page']['media']
+            if not resp['data']['Page']['pageInfo']['hasNextPage']:
+                break
 
 
-def anilist(*a, **k):
-    resp = _anilist(*a, **k)
-
-    try:
-        resp.raise_for_status()
-    except requests.exceptions.HTTPError:
-        new_token_resp = requests.post(
-            URL_FMT.format('auth/access_token'),
-            data={
-                'grant_type': 'client_credentials',
-                'client_id': settings.ANILIST_CLIENT_ID,
-                'client_secret': settings.ANILIST_CLIENT_SECRET,
-            }
-        )
-        new_token_resp.raise_for_status()
-        AUTH_PARAMS['access_token'] = new_token_resp.json()['access_token']
-
-        try:
-            resp = _anilist(*a, **k)
-        except requests.exceptions.HTTPError:
-            raise RuntimeError(resp.json())
-
-    return resp.json()
+def popular_anime():
+    if POPULAR_ANIME:
+        yield from POPULAR_ANIME
+    else:
+        for anime in _popular_anime():
+            POPULAR_ANIME.append(anime)
+            yield anime
 
 
 def anime_description(anime):
     return '\n\n'.join((
-        anime['title_romaji']
-        if anime['title_romaji'] != anime['title_english']
+        anime['title']['romaji']
+        if (
+            anime['title']['english'] and
+            (anime['title']['romaji'] != anime['title']['english'])
+        )
         else '',
 
         ', '.join(anime['genres'])
     ))
 
 
-@source('Anilist', 'Anime by season', 'Older', 'Newer')
+@source('Anilist', 'Anime by year', 'Older', 'Newer')
 @cached('anilist_seasons')
 def anime_by_season():
-    for year in range(1995, datetime.now().year):
-        for season_index, season in enumerate(SEASONS):
-            for anime in anilist(
-                'browse/anime',
-                year=year,
-                seaon=season,
-                sort='popularity-desc',
-            ):
-                yield {
-                    'title': anime['title_english'],
-                    'description': anime_description(anime),
-                    'order': '{}-{}'.format(year, season_index),
-                    'order_display': '{} {}'.format(
-                        season.capitalize(), year,
-                    ),
-                }
+    for anime in popular_anime():
+        yield {
+            'title': anime['title']['english'] or anime['title']['romaji'],
+            'description': anime_description(anime),
+            'order': anime['startDate']['year']
+        }
 
 
 @source('Anilist', 'Anime by user rating', 'Lower rating', 'Higher rating')
 @cached('anilist_ratings')
 def anime_by_user_rating():
-    seen = set()
-
-    for page in range(1, 15):
-        for anime in anilist(
-            'browse/anime',
-            sort='popularity-desc',
-            page=page,
-        ):
-            if anime['id'] in seen:
-                raise ValueError(anime)
-
-            seen.add(anime['id'])
-
-            yield {
-                'title': anime['title_english'],
-                'description': anime_description(anime),
-                'order': anime['average_score'],
-                'order_display': '{:.2f}%'.format(anime['average_score']),
-            }
-
-
-# anime_by_season()  # just to populate the cache if it's not populated
+    for anime in popular_anime():
+        yield {
+            'title': anime['title']['english'],
+            'description': (
+                anime_description(anime) +
+                '\n\n{}'.format(anime['startDate']['year'])
+            ),
+            'order': anime['averageScore']
+        }
