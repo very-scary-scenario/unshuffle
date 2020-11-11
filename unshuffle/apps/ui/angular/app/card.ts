4295db3962e384be4fe2cac8e45b4134a772427d
colons <colons@colons.co>
2020-11-11 13:37:09 +0000

    Make and use a card interface.

---
+++ unshuffle/apps/ui/angular/app/card.ts
@@ -0,0 +1,8 @@
+export interface Card {
+  id: number;
+  title: string;
+  order: string;
+  order_display: string;
+  description: string;
+  secret_description: string;
+}
--- unshuffle/apps/ui/angular/app/deck/deck.component.ts
+++ unshuffle/apps/ui/angular/app/deck/deck.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../card';
 
 @Component({
   selector: 'app-deck',
   templateUrl: './deck.component.html',
 })
 export class DeckComponent implements OnInit {
-  card = "Mario";
+  card: Card = {
+    id: 1,
+    title: "Mario",
+    description: "it's the mario",
+    secret_description: "",
+    order: "1",
+    order_display: "1",
+  }
 
   constructor() { }
 
